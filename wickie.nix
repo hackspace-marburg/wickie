4ab128c1c541006e078d8b74a59a7555fec6d5ed
Lennart <lennart@lnrt.de>
2023-03-14 09:20:11 +0100

    init

---
+++ wickie.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+
+{
+
+  imports =
+    [
+      ./brockman/nix/module.nix
+    ];
+
+  services.brockman = {
+    enable = true;
+    package = pkgs.haskellPackages.callPackage ./brockman {};
+    config = {
+      channel = "#hsmr";
+      irc = {
+        host = "irc.hackint.org";
+        port = 6697;
+        tls = true;
+      };
+      bots = {
+        wickie = {
+          feed = "http://localhost:3162/hsmr-recent-changes.rss";
+        };
+      };
+    };
+  };
+
+  virtualisation.oci-containers.containers."html2rss" = {
+    ports = [ "3162:3000" ];
+    image = "gilcreator/html2rss-web";
+    volumes = [
+      "/etc/nixos/machines/sicily/includes/wickie/html2rss/feeds.yml:/app/config/feeds.yml"
+    ];
+    environment = {
+      RACK_ENV = "production";
+    };
+  };
+
+}
